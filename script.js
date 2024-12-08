b9e3dbe5112061988a1476a22ba2bd54a47fb256
lisaameilia <lisaameiliadesti130504@gmail.com>
2024-12-08 17:16:10 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.dropbtn').forEach(button => {
-    button.addEventListener('click', () => {
-        const dropdown = button.nextElementSibling;
-        dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-    });
-});
